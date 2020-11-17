1bb00cafa1563bde185e4b0e68110feeb714384a
wantotree98 <iddota2toppo@gmail.com>
2020-11-17 19:44:42 +0800

    lab4 commit latest

--- 4.4.server.c
+++ 4.4.server.c
@@ -4,11 +4,16 @@
 #include<arpa/inet.h>
 #include<string.h>
 #include<stdlib.h>
+#include<netinet/in.h>
+#include<sys/types.h>
+
 int main(int argc, char*argv[]){
 
 int socket_desc, new_socket, c;
 struct sockaddr_in server, client;
 char*message;
+int optval;
+socklen_t optlen=sizeof(optval);
 
 //create socket
 
@@ -16,6 +21,23 @@ socket_desc = socket(AF_INET, SOCK_STREAM, 0);
 if(socket_desc == -1){
 printf("Could not create a socket \n");
 }
+
+//check status keepalive option
+if(getsockopt(socket_desc, SOL_SOCKET, SO_KEEPALIVE, &optval, &optlen)<0){
+perror("getsockopt()");
+close(socket_desc);
+exit(EXIT_FAILURE);
+}
+printf("SO_KEEPALIVE IS %s\n", (optval ? "ON":"OFF"));
+
+//set option active
+optval=1;
+optlen=sizeof(optval);
+if(setsockopt(socket_desc, SOL_SOCKET, SO_KEEPALIVE, &optval, optlen)<0){
+perror("setsockopt()");
+close(socket_desc);
+}
+
 
 //prepare sockaddr_in structure
 server.sin_family = AF_INET;
@@ -26,7 +48,7 @@ server.sin_port = htons(8888);
 if(bind(socket_desc, (struct sockaddr*)&server, sizeof(server))<0){
 
 puts("Bind Failed \n");
-return 1;
+
 }
 
 puts("bind done \n");
@@ -37,8 +59,7 @@ listen(socket_desc, 3);
 //accept and incoming connection
 puts("Waiting for incoming connections. . . ");
 c = sizeof(struct sockaddr_in);
-while((new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c))){
-
+while((new_socket = accept(socket_desc, (struct sockaddr*)&client, (socklen_t*)&c))){
 puts("Connection accepted \n");
 message = "Buat apa tu boskiut?! \n";
 write(new_socket, message, strlen(message));
@@ -55,5 +76,3 @@ return 0;
 //printf("Type anything pls: \n",message);
 //send(new_socket, message, strlen(message),0);
 }
-
-
